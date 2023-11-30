79a299481d03a5849470aac4060a993ef3ab5bb7
Asa Adomatis <asaa@clemson.edu>
2023-11-30 12:16:04 -0500

    Began work on Histogram

--- visualizations/scripts/sightings-by-duration.js
+++ visualizations/scripts/sightings-by-duration.js
@@ -1,88 +1,51 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+// // set the dimensions and margins of the graph
+// var margin = {top: 10, right: 30, bottom: 30, left: 40},
+//     width = 460 - margin.left - margin.right,
+//     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#sightings-by-duration")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+// // append the svg object to the body of the page
+// var svg = d3.select("#sightings-by-duration")
+//   .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform",
+//           "translate(" + margin.left + "," + margin.top + ")");
 
 // Read the data and compute summary statistics for each specie
 d3.csv("..\\..\\data\\final-data.csv").then(function(data) {
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.duration; })])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
-  var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
-    .key(d => d["generalizedShape"])
-    .rollup(d => {
-      q1 = d3.quantile(d.map(function(g) { return g.duration;}).sort(d3.ascending),.25)
-      median = d3.quantile(d.map(function(g) { return g.duration;}).sort(d3.ascending),.5)
-      q3 = d3.quantile(d.map(function(g) { return g.duration;}).sort(d3.ascending),.75)
-      interQuantileRange = q3 - q1
-      min = q1 - 1.5 * interQuantileRange
-      max = q3 + 1.5 * interQuantileRange
-      return({q1: q1, median: median, q3: q3, interQuantileRange: interQuantileRange, min: min, max: max})
-    })
-    .entries(data);
-    console.log(sumstat);
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return +d.duration; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(70)); // then the numbers of bins
 
-//   // Show the X scale
-//   var x = d3.scaleBand()
-//     .range([ 0, width ])
-//     .domain(["setosa", "versicolor", "virginica"])
-//     .paddingInner(1)
-//     .paddingOuter(.5)
-//   svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x))
+  // And apply this function to data to get the bins
+  var bins = histogram(data);
 
-//   // Show the Y scale
-//   var y = d3.scaleLinear()
-//     .domain([3,9])
-//     .range([height, 0])
-//   svg.append("g").call(d3.axisLeft(y))
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-//   // Show the main vertical line
-//   svg
-//     .selectAll("vertLines")
-//     .data(sumstat)
-//     .enter()
-//     .append("line")
-//       .attr("x1", function(d){return(x(d.key))})
-//       .attr("x2", function(d){return(x(d.key))})
-//       .attr("y1", function(d){return(y(d.value.min))})
-//       .attr("y2", function(d){return(y(d.value.max))})
-//       .attr("stroke", "black")
-//       .style("width", 40)
-
-//   // rectangle for the main box
-//   var boxWidth = 100
-//   svg
-//     .selectAll("boxes")
-//     .data(sumstat)
-//     .enter()
-//     .append("rect")
-//         .attr("x", function(d){return(x(d.key)-boxWidth/2)})
-//         .attr("y", function(d){return(y(d.value.q3))})
-//         .attr("height", function(d){return(y(d.value.q1)-y(d.value.q3))})
-//         .attr("width", boxWidth )
-//         .attr("stroke", "black")
-//         .style("fill", "#69b3a2")
-
-//   // Show the median
-//   svg
-//     .selectAll("medianLines")
-//     .data(sumstat)
-//     .enter()
-//     .append("line")
-//       .attr("x1", function(d){return(x(d.key)-boxWidth/2) })
-//       .attr("x2", function(d){return(x(d.key)+boxWidth/2) })
-//       .attr("y1", function(d){return(y(d.value.median))})
-//       .attr("y2", function(d){return(y(d.value.median))})
-//       .attr("stroke", "black")
-//       .style("width", 80)
+  // append the bar rectangles to the svg element
+  svg.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
 })
