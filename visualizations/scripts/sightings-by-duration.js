5a511e49268533146244c995a2647a6b08b320f3
Asa Adomatis <asaa@clemson.edu>
2023-11-30 13:16:57 -0500

    Initial duration histogram

--- visualizations/scripts/sightings-by-duration.js
+++ visualizations/scripts/sightings-by-duration.js
@@ -1,51 +1,66 @@
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 40},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
-
-// // append the svg object to the body of the page
-// var svg = d3.select("#sightings-by-duration")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-// Read the data and compute summary statistics for each specie
 d3.csv("..\\..\\data\\final-data.csv").then(function(data) {
-  // X axis: scale and draw:
+  var size = {
+    width: 800,
+    height: 800,
+    top: 50, 
+    right: 50, 
+    bottom: 50, 
+    left: 50
+  };
+
+  var binCount = 100;
+
+  var svg = d3.select("#sightings-by-duration")
+    .attr("width", size.width)
+    .attr("height", size.height)
+    .attr("viewBox", [0, 0, size.width, size.height]);
+
+  var bins = d3.bin()
+    .thresholds(binCount)
+    .value((d) => d.duration)
+    (data);
+
   var x = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.duration; })])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return +d.duration; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
-
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
-
-  // Y axis: scale and draw:
+    .domain([bins[0].x0, bins[bins.length - 1].x1])
+    .range([size.left, size.width - size.right]);
+  
+  // Declare the y (vertical position) scale.
   var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
-      .call(d3.axisLeft(y));
+    .domain([0, d3.max(bins, (d) => d.length)])
+    .range([size.height - size.bottom, size.top]);
 
-  // append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
+  // Add a rect for each bin.
+  svg.append("g")
+    .attr("fill", "steelblue")
+    .selectAll()
+    .data(bins)
+    .join("rect")
+    .attr("x", (d) => x(d.x0) + 1)
+    .attr("width", (d) => x(d.x1) - x(d.x0) - 1)
+    .attr("y", (d) => y(d.length))
+    .attr("height", (d) => y(0) - y(d.length));
+
+    // Add the x-axis and label.
+    svg.append("g")
+    .attr("transform", `translate(0,${size.height - size.bottom})`)
+    .call(d3.axisBottom(x).ticks(size.width / binCount).tickSizeOuter(0))
+    .call((g) => g.append("text")
+        .attr("x", size.width)
+        .attr("y", size.bottom - 4)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "end")
+        .text("Unemployment rate (%) →"));
+
+  // Add the y-axis and label, and remove the domain line.
+  svg.append("g")
+      .attr("transform", `translate(${size.left},0)`)
+      .call(d3.axisLeft(y).ticks(size.height / 40))
+      .call((g) => g.select(".domain").remove())
+      .call((g) => g.append("text")
+          .attr("x", - size.left)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text("↑ Frequency (no. of counties)"));
+
 })
