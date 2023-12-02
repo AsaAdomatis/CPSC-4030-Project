f5438bd699c01e442b52f6103bc239b463847a14
Asa Adomatis <asaa@clemson.edu>
2023-12-02 17:20:26 -0500

    Added filtering to counties

--- visualizations/scripts/sightings-by-county.js
+++ visualizations/scripts/sightings-by-county.js
@@ -1,39 +1,34 @@
+var sbc = {
+    counties: undefined,
+    colorScale: undefined,
+    group: undefined,
+
+    count(county) {
+        if(this.group.has(county)) {
+            return this.group.get(county).length;
+        }
+        return 0;
+    }
+}
+
+function transitionCounty(data) {
+    // setting data up
+    sbc.group = d3.group(data, d => d.county);
+
+    sbc.counties.transition()
+        .attr("fill", d => {
+            let county = d.properties["NAME"] + " County";
+            return sbc.colorScale(sbc.count(county));
+        })
+}
+
+
 d3.csv("..\\..\\data\\final-data.csv").then(function(dataset) {    
     d3.json("..\\..\\data\\us-counties-geo.json").then(function(mapdata){
         // getting a list of all us states and the counts of that generalized shape per sighting
-        const validStates = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'de', 'fl', 'ga', 'hi', 'id', 'il', 
-        'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 
-        'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 
-        'va', 'wa', 'wv', 'wi', 'wy'];
-        var sightings = {};
-        dataset.forEach(d => {
-            var county = d.county;
-            var shape = d.generalizedShape;
-            if (validStates.includes(d.state)) {
-                if (sightings.hasOwnProperty(county)) {
-                    if (sightings[county].hasOwnProperty(shape)) {
-                        sightings[county][shape]++;
-                    }
-                    else {
-                        sightings[county][shape] = 1;
-                    }
-                }                
-                else {
-                    sightings[county] = {};
-                    sightings[county][shape] = 1;
-                }  
-            }   
-        });
-        // adding totals
-        Object.keys(sightings).forEach(function(countyKey, countyIndex) {
-            let total = 0;
-            Object.keys(sightings[countyKey]).forEach(function(shapeKey, shapeIndex) {
-                total += sightings[countyKey][shapeKey];
-            })
-            sightings[countyKey].total = total;
-        });
-
-        
+        dataset = Filters.filterBadDates(dataset);
+        sbc.group = d3.group(dataset, d => d.county);
+  
         var size = {
             width: 800,
             height: 800
@@ -58,21 +53,16 @@ d3.csv("..\\..\\data\\final-data.csv").then(function(dataset) {
             .attr("d", pathGenerator({type: "Sphere"}))
             .attr("fill", "white");
 
-        var max = 0;
-        Object.keys(sightings).forEach(function(key, index) {
-            if (sightings[key].total > max) {                        
-                max = sightings[key].total;
-            }      
-        });
+        let max = d3.max(sbc.group, d => d[1].length);
 
-        var thresholds = [1, 5, 10, 25, 50, 100, 250, 500]
-        var colorScale = d3.scaleThreshold()
+        var thresholds = [0, 5, 10, 25, 50, 100, 250, 500]
+        sbc.colorScale = d3.scaleThreshold()
             .domain(thresholds)
             .range(d3.schemePuBu[thresholds.length]);
 
         var tooltip = d3.select("tooltip")
 
-        var counties = svg.append("g")
+        sbc.counties = svg.append("g")
             .selectAll(".county")
             .data(mapdata.features)
             .enter()
@@ -81,26 +71,17 @@ d3.csv("..\\..\\data\\final-data.csv").then(function(dataset) {
             .attr("d", d => pathGenerator(d))
             .attr("fill", d => {
                 var countyName = d.properties["NAME"] + " County";
-                if(sightings.hasOwnProperty(countyName)) {
-                    return colorScale(+sightings[countyName].total);
-                }
-                else {
-                    return "GhostWhite";
-                }
+                return sbc.colorScale(sbc.count(countyName));
             })
             .on("mouseover", function (d, i) {
                 d3.select(this)
-                    .attr("stroke", "black")
-                var countyName = i.properties["NAME"] + " County"
-                var total = 0
-                if (sightings.hasOwnProperty(countyName)) {
-                    total = +sightings[countyName].total
-                }
+                    .attr("stroke", "black");
+                var countyName = i.properties["NAME"] + " County";
                 tooltip
                     .style("visibility", "visible")
                     .style("left", `${d.x + offset.x}px`)
                     .style("top", `${d.y + offset.y}px`)
-                    .text(`${countyName}: ${total}`)
+                    .text(`${countyName}: ${sbc.count(countyName)}`);
             })
             .on("mouseout", function () {
                 d3.select(this)
